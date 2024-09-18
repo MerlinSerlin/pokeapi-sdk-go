faf7bebc5d9809fc632dd627323577b489aedaff
Marshall Serlin <marshallserlin@gmail.com>
2024-09-18 16:41:53 -0700

    updated tests

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/merlinserlin/pokeapi-sdk-go/api"
+)
+
+func main() {
+	// Initialize the client from the api package
+	client := api.NewClient()
+
+	// Get a Pokémon
+	pokemon, err := client.GetPokemon("pikachu")
+	if err != nil {
+		log.Fatalf("Error getting Pokemon: %v", err)
+	}
+	fmt.Printf("Pokemon: %s, ID: %d\n", pokemon.Name, pokemon.ID)
+
+	// Get a Generation
+	gen, err := client.GetGeneration("1")
+	if err != nil {
+		log.Fatalf("Error getting Generation: %v", err)
+	}
+	fmt.Printf("Generation: %s, Main Region: %s\n", gen.Name, gen.MainRegion.Name)
+}
--- tests/pokemon_test.go
+++ tests/pokemon_test.go
@@ -13,7 +13,64 @@ func TestGetPokemon(t *testing.T) {
 
 	if assert.NoError(t, err) {
 		assert.NotNil(t, pokemon)
-		assert.Equal(t, 35, pokemon.ID) // ID of Pikachu
+		assert.Equal(t, 35, pokemon.ID)
 		assert.Equal(t, "clefairy", pokemon.Name)
+		assert.Equal(t, 113, pokemon.BaseExperience)
+		assert.Equal(t, 6, pokemon.Height)
+		assert.True(t, pokemon.IsDefault)
+		assert.Equal(t, 75, pokemon.Weight)
+
+		// Test Abilities
+		assert.Len(t, pokemon.Abilities, 3)
+		assert.Equal(t, "cute-charm", pokemon.Abilities[0].Ability.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/ability/56/", pokemon.Abilities[0].Ability.URL)
+		assert.False(t, pokemon.Abilities[0].IsHidden)
+		assert.Equal(t, 1, pokemon.Abilities[0].Slot)
+
+		assert.Equal(t, "magic-guard", pokemon.Abilities[1].Ability.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/ability/98/", pokemon.Abilities[1].Ability.URL)
+		assert.False(t, pokemon.Abilities[1].IsHidden)
+		assert.Equal(t, 2, pokemon.Abilities[1].Slot)
+
+		assert.Equal(t, "friend-guard", pokemon.Abilities[2].Ability.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/ability/132/", pokemon.Abilities[2].Ability.URL)
+		assert.True(t, pokemon.Abilities[2].IsHidden)
+		assert.Equal(t, 3, pokemon.Abilities[2].Slot)
+
+		// Test Forms
+		assert.Len(t, pokemon.Forms, 1)
+		assert.Equal(t, "clefairy", pokemon.Forms[0].Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/pokemon-form/35/", pokemon.Forms[0].URL)
+
+		// Test Game Indices
+		assert.Len(t, pokemon.GameIndices, 20)
+		assert.Equal(t, 4, pokemon.GameIndices[0].GameIndex)
+		assert.Equal(t, "red", pokemon.GameIndices[0].Version.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/version/1/", pokemon.GameIndices[0].Version.URL)
+
+		// Test Held Items
+		assert.Len(t, pokemon.HeldItems, 3)
+		assert.Equal(t, "moon-stone", pokemon.HeldItems[0].Item.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/item/81/", pokemon.HeldItems[0].Item.URL)
+		assert.Len(t, pokemon.HeldItems[0].VersionDetails, 22)
+		assert.Equal(t, 5, pokemon.HeldItems[0].VersionDetails[0].Rarity)
+		assert.Equal(t, "ruby", pokemon.HeldItems[0].VersionDetails[0].Version.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/version/7/", pokemon.HeldItems[0].VersionDetails[0].Version.URL)
+
+		assert.Equal(t, "leppa-berry", pokemon.HeldItems[1].Item.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/item/131/", pokemon.HeldItems[1].Item.URL)
+		assert.Len(t, pokemon.HeldItems[1].VersionDetails, 12)
+		assert.Equal(t, 50, pokemon.HeldItems[1].VersionDetails[0].Rarity)
+
+		assert.Equal(t, "comet-shard", pokemon.HeldItems[2].Item.Name)
+		assert.Equal(t, "https://pokeapi.co/api/v2/item/624/", pokemon.HeldItems[2].Item.URL)
+		assert.Len(t, pokemon.HeldItems[2].VersionDetails, 4)
+		assert.Equal(t, 1, pokemon.HeldItems[2].VersionDetails[0].Rarity)
+
+		// Test Location Area Encounters
+		assert.Equal(t, "https://pokeapi.co/api/v2/pokemon/35/encounters", pokemon.LocationAreaEncounters)
+
+		// The rest of the tests go on...
+		// (Species, Sprites, Stats, Types, PastTypes)
 	}
 }
