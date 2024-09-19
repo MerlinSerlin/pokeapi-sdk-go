a027c6e5668915c5a1099e735da67a97f0a2aa78
Marshall Serlin <marshallserlin@gmail.com>
2024-09-19 12:37:26 -0700

    cleanup old code

--- models/generation.go
+++ models/generation.go
@@ -1,34 +1,5 @@
 package models
 
-// type Generation struct {
-// 	ID        int    `json:"id"`
-// 	Name      string `json:"name"`
-// 	Abilities []struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"abilities"`
-// 	MainRegion struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"main_region"`
-// 	Moves []struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"moves"`
-// 	PokemonSpecies []struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"pokemon_species"`
-// 	Types []struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"types"`
-// 	VersionGroups []struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"version_groups"`
-// }
-
 type Generation struct {
 	ID             int                `json:"id"`
 	Name           string             `json:"name"`
